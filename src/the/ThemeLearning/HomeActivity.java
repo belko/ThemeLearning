be43c66d96c84453142fbf815516ebc9913386c1
belko <timur.olifirenko@gmail.com>
2014-03-15 19:41:03 +0200

    javadoc

--- src/the/ThemeLearning/HomeActivity.java
+++ src/the/ThemeLearning/HomeActivity.java
@@ -10,7 +10,7 @@ import android.view.Menu;
  */
 public class HomeActivity extends Activity {
 
-	/*
+	/**
 	 * (non-Javadoc)
 	 * @see android.app.Activity#onCreate(android.os.Bundle)
 	 */
@@ -20,10 +20,25 @@ public class HomeActivity extends Activity {
 		setContentView(R.layout.activity_home);
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * @see android.app.Activity#onCreateOptionsMenu(android.view.Menu)
+	 */
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.home, menu);
+		return true;
+	}
+	
+	/**
+	 * test method
+	 * @param userId user identificator
+	 * @param userName user full name
+	 * @return user is valid
+	 */
+	public boolean testMethod(int userId, String userName)
+	{
 		return true;
 	}
 
